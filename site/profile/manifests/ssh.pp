27ff809aa82bd1cf9200a8a48836a0d04b539ef6
Mariusz86stw <matula.mariusz@gmail.com>
2019-05-09 15:47:17 +0200

    Update ssh.pp

--- site/profile/manifests/ssh.pp
+++ site/profile/manifests/ssh.pp
@@ -1,2 +1,15 @@
-class profile::ssh{
+class profile::ssh {
+  package { 'openssh-server':
+    ensure => present,
+  }
+  service { 'sshd':
+    ensure => 'running'
+    enable => 'true',
+  }
+  ssh_authorized_key { 'root@master.puppet.vm':
+    ensure => present,
+    user => 'root',
+    type => 'ssh-rsa',
+    key => '',TODO
+  }
 }
