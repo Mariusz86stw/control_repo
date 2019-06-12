42d3efa04ebda99bb1a308cae5ba759a047c090c
Mariusz86stw <matula.mariusz@gmail.com>
2019-06-12 13:33:10 +0200

    Update ssh.pp

--- site/profile/manifests/ssh.pp
+++ site/profile/manifests/ssh.pp
@@ -10,6 +10,6 @@ class profile::ssh {
     ensure => present,
     user => 'root',
     type => 'ssh-rsa',
-    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC95RSfBH4aKJVekQQQswHpIdhd9ikNzzs/0yO83eiNLb6Sq+7RztKyQR7lpQvGqRVVeJy7n31A5YsBCmN17kuhszoOfCuo/GlhzGkYe4D1EXbhUzWB2vDHUeND/OG4IpMzbOKRzyJbGqguLMnjPEobmoYF8UO+S8N851p6modixgFQ1d4+iuj/vMvGT3QiIu496vA7bAweuxLowvWYhqmx0UzhyvSXmxFNJmnJrsL2QrW9P6wtXdmd92XJlGgofi7bvUmSlYNFq7Y4G7/+G3BI5PLjAXrpcoNt3RclVZzDgbCN/rqXjVrYZXa5KOMdf0FX7PoN5Nf0JAInjDJS7Rnr',
+    key => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDCigfLvSjXNy6uSBrYAb+KNJdLdb9HG7BhGoXF7skCtwlLBklkqFhkZFRavz5/GOm5MEKczJanfc2rbg1uU/uA+7b0x6yP/cOmDjAamJeTfs1Wjb9YOt3p7tDTkaVLuD3X15xS4AZ142Rhjuq5J4AlgTokT5jbJvWiWNg98li4T6oeW+BGksxnPfwwfCDqeC9X5ppvsNtj8JW2hgo0cvKqMA1HYTn2N2oFDxAmt6aDBeJT+J89Bv/nGUKSJ3SY+Uyr/g8fTdXisZHECB+6L2XG2Fygh7r3nMSKkbuY76I1WfKXSvuop6MmM5+r5dcqLosawxxTJ55ncMbTLHDSyER',
   }
 }
